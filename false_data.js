9a226e7b3026518cd9440475204c9a594deeb319
Matthew Nguyen <matthpn2@uci.edu>
2018-10-26 15:54:26 -0700

    Finalized commit.

---
+++ false_data.js
@@ -0,0 +1,21 @@
+var faker = require('faker');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host        : 'localhost',
+    user        : 'root',
+    password    : 'phUcvu040916',
+    database    : 'join_us'
+});
+
+var data = [];
+for(var i = 0; i < 500; i++) {
+    data.push([ faker.internet.email(), faker.data.past() ]);
+}
+
+var q = 'INSERT INTO users (email, created_at) VALUES ?';
+connection.query(q, [data], function(err, results) {
+    if(err) throw err;
+    console.log(result);
+});
+connection.end();
---
+++ schema.sql
@@ -0,0 +1,55 @@
+-- CREATE and USE JOIN US DATABASE --
+DROP DATABASE IF EXISTS join_us;
+CREATE DATABASE join_us;
+USE join_us;
+
+-- USERS SCHEMA TABLE --
+CREATE TABLE users
+    (
+        email VARCHAR(255) PRIMARY KEY,
+        created_at TIMESTAMP DEFAULT NOW()
+    );
+
+-- FIND EARLIEST DATE A USER JOINED --
+SELECT DATE_FORMAT(created_at, '%M %D %Y') AS earliest_date 
+FROM users ORDER BY created_at 
+LIMIT 1;
+
+SELECT DATE_FORMAT(MIN(created_at), '%M %D %Y') AS earliest_date
+FROM users;
+
+-- FIND EMAIL OF THE FIRST EARLIEST USER --
+SELECT email
+FROM users
+ORDER BY created_at
+LIMIT 1;
+
+SELECT email
+FROM users
+WHERE created_at = (SELECT MIN(created_at) FROM users);
+
+-- FIND USERS ACCORDING TO THE MONTH THEY JOINED --
+SELECT 
+    MONTHNAME(created_at) AS month,
+    COUNT(*) AS count
+FROM users
+GROUP BY month
+ORDER BY count DESC;
+
+-- COUNT NUMBER OF USERS WITH YAHOO EMAILS --
+SELECT COUNT(*) AS yahoo_users
+FROM users
+WHERE email LIKE "%@yahoo.com";
+
+-- CALCULATE TOTAL NUMBER OF USERS FOR EACH EMAIL HOST --
+SELECT
+    CASE
+        WHEN email LIKE "%@gmail.com" THEN 'gmail'
+        WHEN email LIKE "%@yahoo.com" THEN 'yahoo'
+        WHEN email LIKE "%@hotmail.com" THEN 'hotmail'
+        ELSE 'other'
+    END AS provider,
+    COUNT(*) AS total_users
+FROM users
+GROUP BY provider
+ORDER BY total_users DESC;
